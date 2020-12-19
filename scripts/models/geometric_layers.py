c68ae3be45c226cd86098c9fe7ef08513efcfe71
Zhantao Deng <zhantao.deng@epfl.ch>
2020-12-19 17:45:59 +0100

    borrow several functions from the pytorch geometry lib to do rotationMat-quaternion-axisangle convertion

--- scripts/models/geometric_layers.py
+++ scripts/models/geometric_layers.py
@@ -9,14 +9,19 @@ from HMR:
 from VIBE:
     https://github.com/mkocabas/VIBE
     
-Some functions are from MPI:
+Some functions are 
+from MPI:
     Contact: ps-license@tuebingen.mpg.de
+
+from Pytorch Geometric:
+    https://github.com/rusty1s/pytorch_geometric
     
 """
+
 import torch
 from torch.nn import functional as F
 
-def rodrigues(theta):
+def axisAngle_to_rotationMatrix(theta):
     """Convert axis-angle representation to rotation matrix.
     Args:
         theta: size = [B, 3]
@@ -31,6 +36,162 @@ def rodrigues(theta):
     v_sin = torch.sin(angle)
     quat = torch.cat([v_cos, v_sin * normalized], dim = 1)
     return quat2mat(quat)
+
+def rotationMatrix_to_axisAngle(rotation_matrix):
+    """Convert 3x4 rotation matrix to Rodrigues vector
+
+    Args:
+        rotation_matrix (Tensor): rotation matrix.
+
+    Returns:
+        Tensor: Rodrigues vector transformation.
+
+    Shape:
+        - Input: :math:`(N, 3, 4)` or '(N,3,3)'
+        - Output: :math:`(N, 3)`
+
+    Example:
+        >>> input = torch.rand(2, 3, 4)  # Nx4x4
+        >>> output = tgm.rotation_matrix_to_angle_axis(input)  # Nx3
+    """
+    
+    # expand shape
+    if rotation_matrix.shape[2] == 3:
+        expandRMat  = torch.zeros([rotation_matrix.shape[0],3,4])
+        expandRMat[:,:,:-1] = rotation_matrix
+        rotation_matrix = expandRMat
+    
+    # todo add check that matrix is a valid rotation matrix
+    quaternion = rotationMatrix_to_Quaternion(rotation_matrix)
+    return quaternion_to_axisAngle(quaternion)
+
+def quaternion_to_axisAngle(quaternion: torch.Tensor) -> torch.Tensor:
+    """Convert quaternion vector to angle axis of rotation.
+
+    Adapted from ceres C++ library: ceres-solver/include/ceres/rotation.h
+
+    Args:
+        quaternion (torch.Tensor): tensor with quaternions.
+
+    Return:
+        torch.Tensor: tensor with angle axis of rotation.
+
+    Shape:
+        - Input: :math:`(*, 4)` where `*` means, any number of dimensions
+        - Output: :math:`(*, 3)`
+
+    Example:
+        >>> quaternion = torch.rand(2, 4)  # Nx4
+        >>> angle_axis = tgm.quaternion_to_axisAngle(quaternion)  # Nx3
+    """
+    if not torch.is_tensor(quaternion):
+        raise TypeError("Input type is not a torch.Tensor. Got {}".format(
+            type(quaternion)))
+
+    if not quaternion.shape[-1] == 4:
+        raise ValueError("Input must be a tensor of shape Nx4 or 4. Got {}"
+                         .format(quaternion.shape))
+    # unpack input and compute conversion
+    q1: torch.Tensor = quaternion[..., 1]
+    q2: torch.Tensor = quaternion[..., 2]
+    q3: torch.Tensor = quaternion[..., 3]
+    sin_squared_theta: torch.Tensor = q1 * q1 + q2 * q2 + q3 * q3
+
+    sin_theta: torch.Tensor = torch.sqrt(sin_squared_theta)
+    cos_theta: torch.Tensor = quaternion[..., 0]
+    two_theta: torch.Tensor = 2.0 * torch.where(
+        cos_theta < 0.0,
+        torch.atan2(-sin_theta, -cos_theta),
+        torch.atan2(sin_theta, cos_theta))
+
+    k_pos: torch.Tensor = two_theta / sin_theta
+    k_neg: torch.Tensor = 2.0 * torch.ones_like(sin_theta)
+    k: torch.Tensor = torch.where(sin_squared_theta > 0.0, k_pos, k_neg)
+
+    angle_axis: torch.Tensor = torch.zeros_like(quaternion)[..., :3]
+    angle_axis[..., 0] += q1 * k
+    angle_axis[..., 1] += q2 * k
+    angle_axis[..., 2] += q3 * k
+    return angle_axis
+
+def rotationMatrix_to_Quaternion(rotation_matrix, eps=1e-6):
+    """Convert 3x4 rotation matrix to 4d quaternion vector
+
+    This algorithm is based on algorithm described in
+    https://github.com/KieranWynn/pyquaternion/blob/master/pyquaternion/quaternion.py#L201
+
+    Args:
+        rotation_matrix (Tensor): the rotation matrix to convert.
+
+    Return:
+        Tensor: the rotation in quaternion
+
+    Shape:
+        - Input: :math:`(N, 3, 4)`
+        - Output: :math:`(N, 4)`
+
+    Example:
+        >>> input = torch.rand(4, 3, 4)  # Nx3x4
+        >>> output = tgm.rotationMatrix_to_Quaternion(input)  # Nx4
+    """
+    if not torch.is_tensor(rotation_matrix):
+        raise TypeError("Input type is not a torch.Tensor. Got {}".format(
+            type(rotation_matrix)))
+
+    if len(rotation_matrix.shape) > 3:
+        raise ValueError(
+            "Input size must be a three dimensional tensor. Got {}".format(
+                rotation_matrix.shape))
+    if not rotation_matrix.shape[-2:] == (3, 4):
+        raise ValueError(
+            "Input size must be a N x 3 x 4  tensor. Got {}".format(
+                rotation_matrix.shape))
+
+    rmat_t = torch.transpose(rotation_matrix, 1, 2)
+
+    mask_d2 = rmat_t[:, 2, 2] < eps
+
+    mask_d0_d1 = rmat_t[:, 0, 0] > rmat_t[:, 1, 1]
+    mask_d0_nd1 = rmat_t[:, 0, 0] < -rmat_t[:, 1, 1]
+
+    t0 = 1 + rmat_t[:, 0, 0] - rmat_t[:, 1, 1] - rmat_t[:, 2, 2]
+    q0 = torch.stack([rmat_t[:, 1, 2] - rmat_t[:, 2, 1],
+                      t0, rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
+                      rmat_t[:, 2, 0] + rmat_t[:, 0, 2]], -1)
+    t0_rep = t0.repeat(4, 1).t()
+
+    t1 = 1 - rmat_t[:, 0, 0] + rmat_t[:, 1, 1] - rmat_t[:, 2, 2]
+    q1 = torch.stack([rmat_t[:, 2, 0] - rmat_t[:, 0, 2],
+                      rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
+                      t1, rmat_t[:, 1, 2] + rmat_t[:, 2, 1]], -1)
+    t1_rep = t1.repeat(4, 1).t()
+
+    t2 = 1 - rmat_t[:, 0, 0] - rmat_t[:, 1, 1] + rmat_t[:, 2, 2]
+    q2 = torch.stack([rmat_t[:, 0, 1] - rmat_t[:, 1, 0],
+                      rmat_t[:, 2, 0] + rmat_t[:, 0, 2],
+                      rmat_t[:, 1, 2] + rmat_t[:, 2, 1], t2], -1)
+    t2_rep = t2.repeat(4, 1).t()
+
+    t3 = 1 + rmat_t[:, 0, 0] + rmat_t[:, 1, 1] + rmat_t[:, 2, 2]
+    q3 = torch.stack([t3, rmat_t[:, 1, 2] - rmat_t[:, 2, 1],
+                      rmat_t[:, 2, 0] - rmat_t[:, 0, 2],
+                      rmat_t[:, 0, 1] - rmat_t[:, 1, 0]], -1)
+    t3_rep = t3.repeat(4, 1).t()
+
+    mask_c0 = mask_d2 * mask_d0_d1
+    mask_c1 = mask_d2 * (~mask_d0_d1)
+    mask_c2 = (~mask_d2) * mask_d0_nd1
+    mask_c3 = (~mask_d2) * (~mask_d0_nd1)
+    mask_c0 = mask_c0.view(-1, 1).type_as(q0)
+    mask_c1 = mask_c1.view(-1, 1).type_as(q1)
+    mask_c2 = mask_c2.view(-1, 1).type_as(q2)
+    mask_c3 = mask_c3.view(-1, 1).type_as(q3)
+
+    q = q0 * mask_c0 + q1 * mask_c1 + q2 * mask_c2 + q3 * mask_c3
+    q /= torch.sqrt(t0_rep * mask_c0 + t1_rep * mask_c1 +  # noqa
+                    t2_rep * mask_c2 + t3_rep * mask_c3)  # noqa
+    q *= 0.5
+    return q
 
 def quat2mat(quat):
     """Convert quaternion coefficients to rotation matrix.
@@ -68,6 +229,30 @@ def orthographic_projection(X, camera):
     X_2d = (camera[:, :, 0] * X_trans.view(shape[0], -1)).view(shape)
     return X_2d
 
+def axisAngle_to_Rot6d(x):
+    """"Convert axis angle to 6d rotation representation
+    Input:
+        (B,3) Batch of corresponding axis angle
+    Output:
+        (B,6) Batch of 6-D rotation representations
+    
+    """
+    rotMat = axisAngle_to_rotationMatrix(x)
+    
+    return rotMat_to_rot6d(rotMat)
+    
+def rot6d_to_axisAngle(x):
+    """"Convert 6d rotation representation to axis angle
+    Input:
+        (B,6) Batch of 6-D rotation representations
+    Output:
+        (B,3) Batch of corresponding axis angle
+    
+    """
+    rotMat = rot6d_to_rotmat(x)
+    
+    return rotationMatrix_to_axisAngle(rotMat)
+    
 def rotMat_to_rot6d(x): 
     """Convert 3x3 rotation matrix to 6D rotation representation.
     Based on Zhou et al., "On the Continuity of Rotation Representations in Neural Networks", CVPR 2019
@@ -76,8 +261,7 @@ def rotMat_to_rot6d(x):
     Output:
         (B,6) Batch of 6-D rotation representations
     """
-    rot_6d = torch.cat(
-        [x[:,:,0], x[:,:,1]], dim = 0).T.reshape(x.shape[0], -1)
+    rot_6d = x[:,:,:2].reshape(x.shape[0], -1)
     
     return rot_6d
 
@@ -103,3 +287,14 @@ def rot6d_to_rotmat(x):
     rot_mats = torch.stack([b1, b2, b3], dim=-1)
 
     return rot_mats
+
+
+
+# import numpy as np
+
+# axisAngle = torch.Tensor(
+#                 [[np.pi/2, 0, 0], [0, np.pi/2, 0],
+#                   [0, 0, np.pi/2],[np.pi/2, np.pi/6, np.pi/3],
+#                   [np.pi/5, np.pi*2/3, np.pi/4]])
+
+# axisAngle - rot6d_to_axisAngle(axisAngle_to_Rot6d(axisAngle))
