de85f7d74ce3558d3aee4d8edf98fa0fd8d01730
Zhantao Deng <zhantao.deng@epfl.ch>
2020-12-25 17:35:12 +0100

    support inference for the frameViBE strucutre

--- scripts/inference.py
+++ scripts/inference.py
@@ -27,11 +27,29 @@ from torchvision.transforms import Normalize
 from utils.imutils import crop, background_replacing
 from utils import Mesh, CheckpointSaver
 from utils.mesh_util import generateSMPLmesh, create_fullO3DMesh
-from models import GraphCNN, res50_plus_Dec, UNet
+from models import GraphCNN, res50_plus_Dec, UNet, frameVIBE, SMPL
+from models.geometric_layers import rotationMatrix_to_axisAngle, axisAngle_to_Rot6d
 from utils.vis_util import read_Obj
 
 
-def visPrediction(path_object: str, path_SMPLmodel: str, displacement: array,
+def visbodyPrediction(prediction, options, device = 'cuda', ind = 0):
+    
+    prediction = prediction[0]
+    
+    mesh = Mesh(options, options.num_downsampling)
+    faces = torch.cat( options.batch_size * [
+                            mesh.faces.to(device)[None]
+                            ], dim = 0 )
+    faces = faces.type(torch.LongTensor).to(device)
+    
+    smpl = SMPL(options.smpl_model_path, device)
+    body = smpl(prediction['theta'][ind][3:75][None], prediction['theta'][ind][75:][None]).cpu()
+    
+    MeshPred = create_fullO3DMesh(body[0], faces.cpu()[0])    
+    o3d.visualization.draw_geometries([MeshPred])
+    
+    
+def visdispPrediction(path_object: str, path_SMPLmodel: str, displacement: array,
                   chooseUpsample: bool = True):
     # upsampled mesh
     pathtext = pjn(path_object, 'GroundTruth/vertex_colors_oversample_%s.npy'%
@@ -162,23 +180,122 @@ def inference(pathCkp: str, pathImg: str = None, pathBgImg: str = None):
     return prediction, img_in
     
 
+def inference_structure(pathCkp: str, pathImg: str = None, 
+                        pathBgImg: str = None):
+    # Load configuration
+    with open( pjn(pathCkp, 'config.json') , 'r' ) as f:
+        options = json.load(f)
+        options = namedtuple('options', options.keys())(**options)
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # mesh = Mesh(options, options.num_downsampling)
+    
+    # load average pose and shape and convert to camera coodinate;
+    # avg pose is decided by the image id we use for training (0-11) 
+    avgPose_objCoord = np.load(options.MGN_avgPose_path)
+    avgPose_objCoord[:3] = rotationMatrix_to_axisAngle(    # for 0,6, front only
+        torch.tensor([[[1,  0, 0],
+                       [0, -1, 0],
+                       [0,  0,-1]]]))
+    avgPose = \
+        axisAngle_to_Rot6d(
+            torch.Tensor(
+                avgPose_objCoord[None]\
+                .repeat(options.batch_size, axis = 0))
+                .reshape(-1, 3)).reshape(options.batch_size, -1)\
+            .to(device)
+    avgBeta = \
+        torch.Tensor(np.load(options.MGN_avgBeta_path)[None]
+                .repeat(options.batch_size, axis = 0)).to(device)
+    # 1.2755 is for our settings
+    avgCam  = \
+        torch.Tensor([1.2755, 0, 0])[None]\
+                .repeat_interleave(options.batch_size, dim = 0)\
+                .to(device)    # we know the rendered dataset
+    
+    # Create model
+    model = frameVIBE(
+            options.smpl_model_path, 
+            avgPose,
+            avgBeta,
+            avgCam
+            ).to(device)
+    
+    optimizer = torch.optim.Adam(params=list(model.parameters()))
+    models_dict = {options.model: model}
+    optimizers_dict = {'optimizer': optimizer}
+    
+    # Load pretrained model
+    saver = CheckpointSaver(save_dir=options.checkpoint_dir)
+    saver.load_checkpoint(models_dict, optimizers_dict, checkpoint_file=options.checkpoint)
+
+    # Prepare and preprocess input image
+    pathToObj = '/'.join(pathImg.split('/')[:-2])
+    cameraIdx = int(pathImg.split('/')[-1].split('_')[0][6:])
+    with open( pjn( pathToObj,'rendering/camera%d_boundingbox.txt'%(cameraIdx)) ) as f:
+        boundbox = literal_eval(f.readline())
+    IMG_NORM_MEAN = [0.485, 0.456, 0.406]
+    IMG_NORM_STD = [0.229, 0.224, 0.225]
+    normalize_img = Normalize(mean=IMG_NORM_MEAN, std=IMG_NORM_STD)  
+    
+    path_to_rendering = '/'.join(pathImg.split('/')[:-1])
+    cameraPath, lightPath = pathImg.split('/')[-1].split('_')[:2]
+    cameraIdx, _ = int(cameraPath[6:]), int(lightPath[5:])
+    with open( pjn( path_to_rendering,'camera%d_boundingbox.txt'%(cameraIdx)) ) as f:
+        boundbox = literal_eval(f.readline())
+    img  = cv2.imread(pathImg)[:,:,::-1].astype(np.float32)
+    
+    # prepare background
+    if options.replace_background:
+        if pathBgImg is None:
+            bgimages = []
+            for subfolder in sorted(glob( pjn(options.bgimg_dir, 'images/validation/*') )):
+                for subsubfolder in sorted( glob( pjn(subfolder, '*') ) ):
+                    if 'room' in subsubfolder:
+                        bgimages += sorted(glob( pjn(subsubfolder, '*.jpg')) )
+            bgimg =  cv2.imread(bgimages[np.random.randint(0, len(bgimages))])[:,:,::-1].astype(np.float32)
+        else:
+            bgimg = cv2.imread(pathBgImg)[:,:,::-1].astype(np.float32)
+        img = background_replacing(img, bgimg)
+    
+    # augment image
+    center =  [(boundbox[0]+boundbox[2])/2, (boundbox[1]+boundbox[3])/2] 
+    scale  =  max((boundbox[2]-boundbox[0])/200, (boundbox[3]-boundbox[1])/200)
+    img  = crop(img, center, scale, [224, 224], rot=0)
+    
+    # img  = torch.Tensor(img).permute(2,0,1)/255
+    img_in = normalize_img( torch.Tensor(img).permute(2,0,1)/255 )
+    
+    # Inference
+    with torch.no_grad():    # disable grad
+        model.eval()    
+        prediction = model(img_in[None].repeat_interleave(options.batch_size, dim = 0).to(device))
+
+    return prediction, img_in, options
+
 if __name__ == '__main__':
     
     path_to_SMPL  = '../body_model/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl' 
-    path_to_chkpt = '../logs/GCMR_single_brighter_green'
-    path_to_object= '/home/zhantao/Documents/masterProject/DEBOR/datasets/MGN_brighter/125611515860795/'
+    path_to_chkpt = '../logs/local/structure'
+    path_to_object= '../datasets/MGN_brighter_augmented/125611499115817_pose95/'
     path_to_image = pjn(path_to_object, 'rendering/camera0_light0_smpl_registered.png')
-                    
-    prediction, img_in = inference(path_to_chkpt, path_to_image)
-    prediction = prediction[0].detach().cpu()
     
-    if prediction.shape[1] == 3:
-        visPrediction(path_to_object, path_to_SMPL, prediction, False)
-        plt.imshow(img_in.permute(1,2,0))
+    inf_body = True
+    
+    if inf_body:
+        prediction, img_in, options = inference_structure(path_to_chkpt, path_to_image)
+        visbodyPrediction(prediction, options)
     else:
-        plt.imshow(prediction)
-        plt.show()
-        
-        plt.imshow(img_in.permute(1,2,0))
-        plt.show()
+        prediction, img_in = inference(path_to_chkpt, path_to_image)
+        prediction = prediction[0].cpu()
+ 
+        if prediction.shape[1] == 3:
+            visdispPrediction(path_to_object, path_to_SMPL, prediction, False)
+            plt.imshow(img_in.permute(1,2,0))
+        else:
+            plt.imshow(prediction)
+            plt.show()
+            
+            plt.imshow(img_in.permute(1,2,0))
+            plt.show()
         
