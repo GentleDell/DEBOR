0603b41fccc65b3c7409b8dbb3b602fbf59a9012
Zhantao Deng <zhantao.deng@epfl.ch>
2020-12-29 22:50:42 +0100

    add cloth regressor and texture regressor 1. the feature from resnet50 is used for pose, cam and shape prediction and cloth + texture prediction; 2. given the predicted pose and camera we use a renderer(can be differentiable) to render the uv coordinate to image and unwarp the image color to the texture uv space for prediction

--- scripts/models/regressor.py
+++ scripts/models/regressor.py
@@ -11,23 +11,62 @@ from .smpl import SMPL
 from .resnet import resnet50
 from .geometric_layers import rot6d_to_axisAngle, rot6d_to_rotmat
 from .camera import cameraPerspective as Cam
-
+from .graph_cnn import GraphCNN
+from .unet import UNet
+from utils import renderer
 
 class frameVIBE(nn.Module):
     def __init__(
             self,
-            model_file, 
+            model_file,
+            mesh,
             init_pose, 
             init_shape, 
-            init_cam
+            init_cam,
+            GCN_channels,
+            GCN_layers,
+            smpl_verts_uvs,
+            smpl_tri_ind
     ):
         super(frameVIBE, self).__init__()
-
+        
+        # image encoder to extract latent feature vector
         self.encoder = ImageEncoder()
 
         # regressor can predict cam, pose and shape params in an iterative way
         self.regressor = Regressor_pose(model_file, init_pose, init_shape, init_cam)
+        
+        # cloth regressor        
+        self.GCN_regressor = GraphCNN(mesh.adjmat, mesh.ref_vertices.t(), 2048, GCN_layers, GCN_channels)
 
+        # texture regressor
+        self.tex_regressor = UNet(input_shape = (224, 224, 3), output_dims = 3)   
+
+        # <=========== rendering parameters
+        # differentiable renderer
+        self.renderer = renderer(batch_size = 1)
+        
+        # an image of uv coordinate as texture to unwarp image to uv map
+        u = torch.arange(start=0, end=1, step=1/224)
+        v = torch.arange(start=0, end=1, step=1/224)
+        grid_v, grid_u = torch.meshgrid(u, v)
+        uvImage = torch.ones([224,224,3])
+        uvImage[:,:,0], uvImage[:,:,1] = grid_u, 1-grid_v    # x,y i.e. col, row
+        self.register_buffer('uvImage', uvImage[None])
+        
+        # auxiliary parameters for rendering
+        faces = mesh.faces.type(torch.LongTensor)
+        R = torch.eye(3)[None]
+        f = torch.ones([1,1])*1000
+        C = torch.ones([1,2])*112
+        self.camRes = 224
+        self.register_buffer('faces', faces[None])
+        self.register_buffer('smpl_verts_uvs', smpl_verts_uvs[None])
+        self.register_buffer('smpl_tri_ind', smpl_tri_ind[None])
+        self.register_buffer('camR', R)
+        self.register_buffer('camf', f)
+        self.register_buffer('camC', C)
+    
     def forward(self, img):
         # input size NTF
         batch_size = img.shape[0]
@@ -36,14 +75,72 @@ class frameVIBE(nn.Module):
         feature = feature.reshape(-1, feature.size(-1))
 
         smpl_output = self.regressor(feature)
-        for s in smpl_output:
+        disp_output = self.GCN_regressor(feature)
+
+        with torch.no_grad():
+            uvimages  = self.render_uvpose(smpl_output[0]['verts'], 
+                                           smpl_output[0]['theta'][:,:3])
+            unwrapTex = self.unwarp(img, uvimages)
+        tex_output  = self.tex_regressor(unwrapTex.permute(0,3,1,2))    # N*3*224*224
+            
+        for ind, s in enumerate(smpl_output):
             s['theta'] = s['theta'].reshape(batch_size, -1)
             s['verts'] = s['verts'].reshape(batch_size, -1, 3)
             s['kp_2d'] = s['kp_2d'].reshape(batch_size, -1, 2)
             s['kp_3d'] = s['kp_3d'].reshape(batch_size, -1, 3)
-            s['rotmat'] = s['rotmat'].reshape(batch_size, -1, 3, 3)
-
+            s['rotmat'] = s['rotmat'].reshape(batch_size, -1, 3, 3)        
+            s['verts_disp'] = disp_output.reshape(batch_size, -1, 3)
+            s['tex_image'] = tex_output.reshape(batch_size, 224, 224, 3)
         return smpl_output
+    
+    def render_uvpose(self, verts, trans):
+        
+        images = []  
+        device = verts.device
+        
+        predTrans = torch.stack(
+            [trans[:, 1],
+             trans[:, 2],
+             2 * 1000. / (224. * trans[:, 0] + 1e-9)], dim=-1)
+        
+        for cnt in range(verts.shape[0]): 
+            image = self.renderer(
+                verts = verts[cnt][None],
+                faces = self.faces,
+                verts_uvs = self.smpl_verts_uvs,
+                faces_uvs = self.smpl_tri_ind,
+                tex_image = self.uvImage,
+                R = self.camR,
+                T = predTrans[cnt][None],
+                f = self.camf,
+                C = self.camC,
+                imgres = self.camRes
+            )
+            
+            images.append(image)
+        
+        return torch.cat(images, dim=0).to(device)
+
+    def unwarp(self, rgbImage, uvimage):
+        
+        textures = []
+        device = rgbImage.device
+        
+        for cnt in range(rgbImage.shape[0]):
+            uv = uvimage[cnt,:,:,:3]
+            rgb = rgbImage[cnt].permute(1,2,0)
+            
+            mask = uv.sum(dim = 2) > 0
+            rgb  = rgb[mask,:]
+            uvCd = (uv[mask][:,:2]*224).round().long()
+            
+            # better to use inverse mapping
+            tex  = torch.zeros_like(uv)
+            tex[uvCd[:,1], uvCd[:,0]] = rgb
+            
+            textures.append(tex)
+            
+        return torch.stack(textures, dim=0).to(device)
 
 class ImageEncoder(nn.Module):
     def __init__(self):
