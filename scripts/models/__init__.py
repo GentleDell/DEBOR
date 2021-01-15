c9caabdb4efb2f7b6112a30184a5a7361cb20bed
Zhantao Deng <zhantao.deng@epfl.ch>
2021-01-15 18:05:40 +0100

    clean related __init__ files

--- scripts/models/__init__.py
+++ scripts/models/__init__.py
@@ -1,7 +1,5 @@
 from .graph_cnn import GraphCNN
 from .smpl import SMPL
-from .cmr import CMR
-from .sizer_nn import res50_plus_Dec, Discriminator_size
 from .unet import UNet
 from .loss import lossfunc as Loss
 from .camera import cameraPerspective as camera
